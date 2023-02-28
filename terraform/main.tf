ddb1d6137d57e5b76d02ac0e024e07d07b6e3270
Luke Meyrick <luke@meyrick.org.uk>
2023-02-28 21:52:18 +0000

    Removing subnet creation and var

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,27 +18,11 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_vpc" "minimal" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "minimal"
-  }
-}
-
-resource "aws_subnet" "minimal_subnet" {
-  vpc_id            = aws_vpc.minimal.id
-  cidr_block        = var.subnet_id
-  availability_zone = "eu-west-2a"
-
-  tags = {
-    Name = "minimal-subnet"
-  }
-}
 
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.minimal_subnet.id
+  subnet_id     = "subnet-02e5b7a43cd28220c"
   tags = {
     Name = "example_ec2_instance"
   }
