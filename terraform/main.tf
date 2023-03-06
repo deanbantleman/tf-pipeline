755a134ac4c4b3b6691925189b99a6a5f74a3cf8
Luke <luke@meyrick.io>
2023-03-06 17:39:22 +0000

    Remove instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,15 +29,3 @@ resource "aws_instance" "example" {
     Department = "CloudOffice"
   }
 }
-
-
-resource "aws_instance" "demo" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-02e5b7a43cd28220c"
-  tags = {
-    Name = "demo_instance_tsb",
-    Terraform = "true"
-    Department = "CloudOffice"
-  }
-}
