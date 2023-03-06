53c096709eb1553cd30ab7f2a259bdd0f3c674e0
Luke <luke@meyrick.io>
2023-03-06 16:55:13 +0000

    setup bucket in tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
+terraform {  
+  backend "s3" {    
+    bucket         = "db-tsbpoc-terraform-state"
+    acl            = "private"
+    encrypt        = true
+    key            = "terraform.tfstate"
+    dynamodb_table = "db-tsb-jenkins-poc-locks"
+    region         = "eu-west-2"  
+  }
+}
+
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -26,23 +38,13 @@ resource "aws_instance" "example" {
   }
 }
 
+
 resource "aws_instance" "demo" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id     = "subnet-02e5b7a43cd28220c"
   tags = {
     Name = "demo_instance_tsb",
-    Terraform = "true"
-    Department = "CloudOffice"
-  }
-}
-
-resource "aws_instance" "test" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-02e5b7a43cd28220c"
-  tags = {
-    Name = "test_instance_tsb",
     Terraform = "true"
     Department = "CloudOffice"
   }
